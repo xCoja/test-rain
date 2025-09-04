e05263b12a450fcfbd1176b71baf4c5a089ac473
Coja <debeljak644644@gmail.com>
2025-09-04 17:16:18 +0200

    d

--- public/clash.js
+++ public/clash.js
@@ -24,10 +24,7 @@ function countdown() {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
+        
     }, 1000);
 }
 
