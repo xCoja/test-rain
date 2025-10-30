6b5dc9bca366801b825877bcc0b2d23b399000e1
Coja <debeljak644644@gmail.com>
2025-10-30 06:59:35 +0100

    d

--- public/csbattle.js
+++ public/csbattle.js
@@ -24,10 +24,10 @@ function countdown() {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
+        // document.getElementById("days").innerHTML = days;
+        // document.getElementById("hours").innerHTML = hours;
+        // document.getElementById("minutes").innerHTML = minutes;
+        // document.getElementById("seconds").innerHTML = seconds;
     }, 1000);
 }
 
